61c62544c2048103b264b06bbc657f9ee04186a9
mimpho-s <u18147675@tuks.co.za>
2026-01-16 01:29:14 +0200

    Loops

---
+++ Loops/99_bottles.py
@@ -0,0 +1,3 @@
+
+for x in range(99, 0, -1):
+  print(f'{x} bottles of beer on the wall\n{x} bottles of beer\nTake one down, pass it around\n')
---
+++ Loops/Fizz_buzz.py
@@ -0,0 +1,22 @@
+
+#This is what I used to understand the solution, but I failed mulitple times to produce the full solution till I understood my mistakes
+'''for x in range(1,10):
+    if x*3:
+        print('Fizz')
+for x in range(1,10):
+    if x*5:
+        print("Buzz")
+for x in range(1,10):
+    if x*3 and x*5:
+        print('FizzBuzz')'''
+
+for x in range(1, 101):
+    if x % 3 == 0 and x % 5 == 0:
+        print('FizzBuzz')
+    elif x % 5 == 0:
+        print("Buzz")
+    elif x % 3 == 0:
+        print('Fizz')
+    else:
+        print(x)
+    
